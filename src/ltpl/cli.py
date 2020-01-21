18bd8f8202412be15c6e770e8dc0a732345a86e1
min.du <min.du@horizon.ai>
2020-01-21 15:19:36 +0800

    init

---
+++ src/ltpl/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+ltpl.cli
+
+a simple command line to run ltpl app.
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+
+def main():
+    print("hello, ltpl")
+
+
+if __name__ == "__main__":
+    main()
